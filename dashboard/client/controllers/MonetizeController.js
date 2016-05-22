46146ba537c29d83e53a7cb7de33d21629706d06
brtwister <bruno.vercosam@gmail.com>
2016-05-22 18:35:05 -0300

    last commit

--- dashboard/client/controllers/MonetizeController.js
+++ dashboard/client/controllers/MonetizeController.js
@@ -4,66 +4,49 @@ app.controller('MonetizeController', function ($scope, $timeout, $http, Service)
     var self = this;
 
 
-    self.currentCampaign = {};
-    self.campaigns = [];
+    self.currentMonetize = {};
+    self.monetizes = [];
     self.isEditing = false;
 
     self.changeStatus = function(){
 
     }
 
-    self.removeCampaignImage = function(campaign){
-        campaign.img = null;
-    }
-    self.getCurrentImage = function(campaign){
-        var i = (campaign.img != null && campaign.img != '')
-            ? campaign.img : 'images/no_image.png';
+    self.getCurrentIcon = function(monetize){
+        var i = (monetize.icon != null && monetize.icon != '')
+            ? monetize.icon : 'images/no_image.png';
         return i;
     }
-
-    $scope.setCampaignImage = function(img){
-        if (img && img[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $timeout(function(){
-                    self.currentCampaign.img = e.target.result;
-                },0);
-
-            }
-            reader.readAsDataURL(img[0]);
-        }
-
-    }
-
-    self.editCampaign = function(campaign){
+    
+    self.editMonetize = function(monetize){
         self.isEditing = true;
-        self.currentCampaign = campaign;
+        self.currentMonetize = monetize;
     };
-    self.newCampaign = function(){
+    self.newMonetize = function(){
         self.isEditing = true;
-        self.currentCampaign = {};
+        self.currentMonetize = {};
     }
 
-    self.cancelEditCampaign = function(campaign){
+    self.cancelEditMonetize = function(monetize){
         self.isEditing = false
-        self.currentCampaign = {};
+        self.currentMonetize = {};
     };
 
-    self.removeCampaign = function (campaign, showdialog) {
+    self.removeMonetize = function (monetize, showdialog) {
         if(!showdialog || confirm("Are you sure you want to delete this item?")) {
-            $http.delete(MONETIZE_URL + '/' + campaign.id)
+            $http.delete(MONETIZE_URL + '/' + monetize.id)
                 .then(function (response) {
                     if(response.data.status) {
 
-                        for (n in self.campaigns) {
-                            if (self.campaigns[n].id == campaign.id) {
-                                self.campaigns.splice(n, 1);
+                        for (n in self.monetizes) {
+                            if (self.monetizes[n].id == monetize.id) {
+                                self.monetizes.splice(n, 1);
                                 break;
                             }
                         }
 
                         self.isEditing = false;
-                        self.currentCampaign = {};
+                        self.currentMonetize = {};
 
                     }else{
                         alert(response.data.msg);
@@ -72,24 +55,24 @@ app.controller('MonetizeController', function ($scope, $timeout, $http, Service)
                 })
                 .catch(function () {
                     if (confirm("An unexpected erro happened. Try Again?")) {
-                        self.removeCampaign(campaign, false);
+                        self.removeMonetize(monetize, false);
                     }
                 });
         }
 
     }
 
-    self.saveCurrentCampaign = function(campaign){
+    self.savecurrentMonetize = function(monetize){
 
-        if(isValid(campaign.appid) && isValid(campaign.status) ) {
-            campaign.platform = parseInt(campaign.platform);
-            campaign.category = parseInt(campaign.category);
-            if(!campaign.id) {
-                $http.post(MONETIZE_URL, campaign)
+        if(isValid(monetize.appid) && isValid(monetize.status) ) {
+            monetize.platform = parseInt(monetize.platform);
+            monetize.category = parseInt(monetize.category);
+            if(!monetize.id) {
+                $http.post(MONETIZE_URL, monetize)
                     .then(function (response) {
                         if(response.data.status) {
-                            self.requestCampaign();
-                            self.currentCampaign = {};
+                            self.requestMonetize();
+                            self.currentMonetize = {};
                         }else{
                             alert(response.data.msg);
                         }
@@ -97,37 +80,37 @@ app.controller('MonetizeController', function ($scope, $timeout, $http, Service)
                     })
                     .catch(function () {
                         if (confirm("An unexpected erro happened. Try Again?")) {
-                            self.saveCurrentCampaign(campaign);
+                            self.savecurrentMonetize(monetize);
                         }
                     });
             }else{
-                $http.put(MONETIZE_URL + '/' + campaign.id, campaign)
+                $http.put(MONETIZE_URL + '/' + monetize.id, monetize)
                     .then(function (response) {
                         if(response.data.status) {
-                            self.requestCampaign();
-                            self.currentCampaign = {};
+                            self.requestMonetize();
+                            self.currentMonetize = {};
                         }else{
                             alert(response.data.msg);
                         }
                     })
                     .catch(function () {
                         if (confirm("An unexpected erro happened. Try Again?")) {
-                            self.saveCurrentCampaign(campaign);
+                            self.savecurrentMonetize(monetize);
                         }
                     });
             }
         }
     };
 
-    self.requestCampaign = function () {
+    self.requestMonetize = function () {
         $http.get(MONETIZE_URL)
             .then(function(response){
-                self.campaigns = response.data.list;
+                self.monetizes = response.data.list;
                 NProgress.done();
             })
             .catch(function(){
                 if(confirm("An unexpected erro happened. Try Again?")){
-                    self.requestCampaign();
+                    self.requestMonetize();
                 }
             });
     };
@@ -135,7 +118,7 @@ app.controller('MonetizeController', function ($scope, $timeout, $http, Service)
     self.init = function () {
         Service.setView('monetize');
         NProgress.start();
-        self.requestCampaign();
+        self.requestMonetize();
     };
 
 });
