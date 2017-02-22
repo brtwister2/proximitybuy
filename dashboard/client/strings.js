c1626b3c25f11a035e33037a6ad5b06507e59966
brtwister <bruno.vercosam@gmail.com>
2017-02-22 02:15:39 -0300

    go path

--- dashboard/client/strings.js
+++ dashboard/client/strings.js
@@ -1,7 +1,7 @@
 /**
  * Created by Bruno on 5/22/2016.
  */
-var HOST = 'http://boomsoft.com.br/proximitybuy';
+var HOST = 'http://138.197.118.32/proximitybuy/dashboard'; //'http://boomsoft.com.br/proximitybuy';
 var CAMPAIGN_URL = HOST + '/api/campaign';
 var MONETIZE_URL = HOST + '/api/application';
 var LOGIN_URL = 'mocks/login.json';  //HOST + '/api/login';
